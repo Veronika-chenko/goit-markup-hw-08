05e795eca3bad074e5bab591a1908458c6e27755
Veronika Kravchenko <aniregma@gmail.com>
2022-12-05 23:30:14 +0200

    refactor

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,32 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector(".menu-open-btn"),
-    closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".mob-menu"),
-    body: document.querySelector("body"),
-  };
+const refs = {
+  openMenuBtn: document.querySelector(".menu-open-btn"),
+  closeMenuBtn: document.querySelector(".menu-close-btn"),
+  menu: document.querySelector(".mob-menu"),
+  body: document.querySelector("body"),
+  anchors: document.querySelectorAll('a[href*="#"]'),
+};
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+refs.openMenuBtn.addEventListener("click", toggleMenu);
+refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
-})();
+function toggleMenu() {
+  refs.menu.classList.toggle("is-hidden");
+  refs.body.classList.toggle("no-scroll");
+}
+
+for (let anchor of refs.anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.currentTarget.blur();
+    const blockId = anchor.getAttribute('href');
+    document.querySelector('' + blockId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      closeMenu: closeMenu(e),
+    })
+  })
+}
+
+function closeMenu(e) {
+  if (e.target.closest('.mob-menu-item')) toggleMenu();
+}
