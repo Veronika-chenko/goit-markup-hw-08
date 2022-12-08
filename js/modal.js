1056dd0b5bb73c34719311bc284dc6cb44879a67
Veronika Kravchenko <aniregma@gmail.com>
2022-12-08 11:49:38 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -6,11 +6,33 @@
     body: document.querySelector("body"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", onOpenModal);
+  refs.closeModalBtn.addEventListener("click", onCloseModal);
+  refs.modal.addEventListener("click", onBackdropClick);
+
+  function onOpenModal() {
+    window.addEventListener("keydown", onEscKeyPress);
+    toggleModal();
+  }
+
+  function onCloseModal() {
+    window.removeEventListener("keydown", onEscKeyPress);
+    toggleModal();
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
     refs.body.classList.toggle("no-scroll");
+  }
+
+  function onBackdropClick(e) {
+    if (e.currentTarget === e.target) toggleModal();
+  }
+
+  function onEscKeyPress(e) {
+    if (e.code === "Escape") {
+      onCloseModal();
+      refs.openModalBtn.blur();
+    }
   }
 })();
