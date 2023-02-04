52cfb4655a3f9cabbfc591625b056e42ee6f4b15
Veronika Kravchenko <aniregma@gmail.com>
2023-02-04 13:46:09 +0200

    feature/works-filter

---
+++ js/portfolio-filter.js
@@ -0,0 +1,42 @@
+(() => {
+    const refs = {
+        btnList: [...document.querySelectorAll('.works-button')],
+        workItems: [...document.querySelectorAll('.works-item')],
+    };
+
+    let currentBtn = document.querySelector('.works-button.current')
+    
+    for (const btn of refs.btnList) {
+        const btnType = btn.dataset.type;
+    
+        btn.addEventListener('click', () => {
+            if(currentBtn === btn) {
+                console.log('the same');
+                return;
+            };
+
+            currentBtn.classList.remove('current');
+            currentBtn = btn;
+            currentBtn.classList.add('current');
+
+            if(btnType === 'all') {
+                for(const item of refs.workItems) {
+                    item.classList.remove('is-hidden');
+                };
+                return;
+            }
+
+            for(const item of refs.workItems) {
+                const intemType = item.classList.contains(btnType);
+
+                if(intemType) {
+                    item.classList.remove('is-hidden');
+                } else {
+                    item.classList.add('is-hidden');
+                };
+            };
+        });
+    };
+})();
+
+
